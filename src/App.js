82b6042221784cfbc1711cafdc1ad5a22d5e7f3a
Ajay <ajay@anniecannons.com>
2020-06-19 19:22:12 -0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Blog from "./Pages/Blog";
+import Login from "./Pages/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/blog" component={Blog} />
+        <Route exact path="/login" component={Login} />
+      </Router>
+    );
+  }
 }
 
 export default App;
